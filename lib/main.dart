4c102253c1e8dcb24812f8daa529ebf8ae89fe94
yusufkorkmaz <yusufkorkmazz98@hotmail.com>
2022-09-21 13:43:16 +0300

    files added

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Game());
+}
+
+class Game extends StatelessWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
+
