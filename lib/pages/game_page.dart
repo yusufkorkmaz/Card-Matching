922a5f594d4ebdf0cad2facf0390c83068759704
yusufkorkmaz <yusufkorkmazz98@hotmail.com>
2022-09-23 21:32:59 +0300

    - Score increased when cards are same

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -17,6 +17,8 @@ class _GamePageState extends State<GamePage> {
   var randomNumber = Random();
   var cardsOnScreen = List<Widget>.filled(getTotalCardCount, Container());
   String clickedCardName = '';
+  int score = 0;
+  List<String> clickedCards = [];
 
   randomImageNumberChoose() {
     List<int> randomImageNumbers = [];
@@ -45,21 +47,36 @@ class _GamePageState extends State<GamePage> {
     return randomIndexNumbers;
   }
 
+  afterClickedOneCard(String imageNameInClickedCard) {
+    setState(() => {
+          clickedCardName = imageNameInClickedCard,
+          clickedCards.add(clickedCardName),
+          print(clickedCards),
+          if (clickedCards.length == 2)
+            {
+              if (clickedCards[0] == clickedCards[1])
+                {
+                  score += 10,
+                  clickedCards = [],
+                }
+              else
+                {
+
+                  clickedCards = [],
+                }
+            }
+        });
+  }
+
   generateCards(List<int> randomImageNumbers, List<int> randomIndexNumbers) {
     int imageNumberIndex = 0;
     for (int i = 0; i < getTotalCardCount; i += 2) {
       cardsOnScreen[randomIndexNumbers[i]] = CustomCard(
-        imageName: '${randomImageNumbers[imageNumberIndex]}.png',
-        clickedCardNameFunc: (val) {
-           setState(() => {clickedCardName = val, print(clickedCardName)});
-        },
-      );
+          imageName: '${randomImageNumbers[imageNumberIndex]}.png',
+          clickedCardNameFunc: (imageName) => afterClickedOneCard(imageName));
       cardsOnScreen[randomIndexNumbers[i + 1]] = CustomCard(
-        imageName: '${randomImageNumbers[imageNumberIndex]}.png',
-        clickedCardNameFunc: (val) {
-          setState(() => {clickedCardName = val,print(clickedCardName)});
-        },
-      );
+          imageName: '${randomImageNumbers[imageNumberIndex]}.png',
+          clickedCardNameFunc: (imageName) => afterClickedOneCard(imageName));
       imageNumberIndex++;
     }
   }
@@ -72,20 +89,34 @@ class _GamePageState extends State<GamePage> {
     super.initState();
   }
 
+  get scoreText => Padding(
+    padding: const EdgeInsets.all(12),
+    child: Text(
+      'Score: $score',
+      style: TextStyle(fontSize: 40),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.amber, title: Text('Card Matching Game'),),
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('Card Matching Game'),
+      ),
       body: Center(
-        child: GridView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: cardsOnScreen.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) => cardsOnScreen[index],
-        ),
+        child: Column(children: [
+          scoreText,
+          GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: cardsOnScreen.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) => cardsOnScreen[index],
+          )
+        ]),
       ),
     );
   }
